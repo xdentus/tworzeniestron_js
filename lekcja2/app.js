501f7ef8f3a9178d987b036e4a659baac569fbf5
xdentus <xdentus@outlook.com>
2023-09-29 12:30:26 +0200

    new files

---
+++ lekcja2/app.js
@@ -0,0 +1,12 @@
+// zadanie 1
+document.querySelector("header").style.backgroundColor = "yellow"
+
+//zadanie 2
+var text = document.querySelector("header h1")
+text.innerHTML = "Preambuła"
+text.style.fontFamily = "Arial Black"
+
+var sections = document.querySelectorAll("section")
+for (const el in sections) {
+    el.style.border = "2px solid brown"
+}
