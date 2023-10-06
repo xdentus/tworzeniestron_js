0a1b99a3bea117651ad8ca65fefb3f31e7c1b3a4
xdentus <xdentus@outlook.com>
2023-10-06 13:48:28 +0200

    another done

--- lekcja4/app.js
+++ lekcja4/app.js
@@ -1,8 +1,26 @@
-let text = document.querySelector('#text')
-let changeBtn = document.querySelector('#change-btn')
-text.innerText =
-  'Za pomocą metody innerText, zmień zawartość tego elementu <div> na: <i>Świetnie!</i> '
+let textArea = document.querySelector('#textAr')
+let textInput = document.querySelector('#textIn')
+let selectColor = document.querySelector('#selectColors')
+let textToChange = document.querySelector('#textToChange')
 
-changeBtn.addEventListener('click', function () {
-  text.innerText = '<i>Świetnie!</i>'
-})
+function changeBg() {
+  textArea.style.background = 'yellow'
+}
+
+function upperCase() {
+  let textHolder = textInput.value.toUpperCase()
+  textInput.value = textHolder
+}
+
+function changeColor() {
+  let color = selectColor.value
+  selectColor.style.color = color
+}
+
+function largeText() {
+  textToChange.style.fontSize = 'xx-large'
+}
+
+function mediumText() {
+  textToChange.style.fontSize = 'medium'
+}
