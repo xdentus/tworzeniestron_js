e42ba4ce0fde966bef1b7cc7968bf052bc136df9
xdentus <xdentus@outlook.com>
2023-10-13 13:27:23 +0200

    done

--- lekcja6/app.js
+++ lekcja6/app.js
@@ -1,15 +1,24 @@
 let button = document.querySelector('#generate')
 let songStorage = document.querySelector('div')
-let newInput = document.createElement("input[type = 'text']")
 
 function generateFields() {
-  //   console.log('dziala')
-  // input[type="hidden"]
   let data = document.querySelector("input[type = 'hidden']").dataset
-  console.log(data)
-  console.log(songStorage)
 
-  songStorage.appendChild(newInput)
+  let newInput1 = document.createElement('input')
+  newInput1.setAttribute('type', 'text')
+  newInput1.setAttribute('value', data.song1)
+  let newInput2 = document.createElement('input')
+  newInput2.setAttribute('type', 'text')
+  newInput2.setAttribute('value', data.song2)
+  let newInput3 = document.createElement('input')
+  newInput3.setAttribute('type', 'text')
+  newInput3.setAttribute('value', data.song3)
+
+  console.log(data)
+
+  songStorage.appendChild(newInput1)
+  songStorage.appendChild(newInput2)
+  songStorage.appendChild(newInput3)
 
   //   console.log(document.querySelector("input[type = 'hidden']").dataset)
 }
